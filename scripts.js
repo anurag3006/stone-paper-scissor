27f77cb8bb1c6e983545b1735d7a38c5a0dcf433
Anurag Kumar <kanurag141@gmail.com>
2023-08-14 00:15:57 +0530

    wrote the rps game(incomplete)

--- scripts.js
+++ scripts.js
@@ -1,25 +1,87 @@
-const choices = ['Rock', 'Paper' , 'Scissor'];
+const choices = ["Rock", "Paper", "Scissor"];
 
 // function to get the coumputer's choice
 function getComputerChoice() {
-    let ans = choices[Math.floor(Math.random()*choices.length)];
-    return ans;
+  let ans = choices[Math.floor(Math.random() * choices.length)];
+  return ans;
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
+var computerSelection = getComputerChoice();
+var playerSelection = prompt("type your choice");
 
 // function for single round of game
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = computerSelection.toUpperCase();
+  playerSelection = playerSelection.toUpperCase();
+  computerSelection = computerSelection.toUpperCase();
 
-    if(playerSelection === "ROCK" && computerSelection === "PAPER"){
-        console.log("Lost ! ");
+  var result = ` `;
+
+  if (playerSelection === "ROCK") {
+    switch (computerSelection) {
+      case "PAPER":
+        result = `You Lost !!
+                          ${computerSelection} beats ${playerSelection}.`;
+        break;
+      case "SCISSOR":
+        result = `You Won !!
+                          ${computerSelection} does not beats ${playerSelection}.`;
+        break;
+
+      default:
+        result = "Tie !!";
+        break;
     }
+  } else if (playerSelection === "PAPER") {
+    switch (computerSelection) {
+      case "SCISSOR":
+        result = `You Lost !!
+                          ${computerSelection} beats ${playerSelection}.`;
+        break;
+      case "ROCK":
+        result = `You Won !!
+                          ${computerSelection} does not beats ${playerSelection}.`;
+        break;
+
+      default:
+        result = "Tie !!";
+        break;
+    }
+  } else {
+    switch (computerSelection) {
+      case "ROCK":
+        result = `You Lost !!
+                          ${computerSelection} beats ${playerSelection}.`;
+        break;
+      case "PAPER":
+        result = `You Won !!
+                          ${computerSelection} does not beats ${playerSelection}.`;
+        break;
+
+      default:
+        result = "Tie !!";
+        break;
+    }
+  }
+
+  return result;
 }
 
-console.log(computerSelection);
+console.log("comp : "+computerSelection);
 
-playRound(playerSelection,computerSelection);
+// let r = playRound(playerSelection, computerSelection);
+// console.log(r);
+
+// playing the above rock-paper-scossor for N number of time;
+function game(playRound, NumberOfTimes) {
+  var score = 0; // final score
+
+  for (let i = 0; i < NumberOfTimes; i++) {
+    let r = playRound(playerSelection, computerSelection);
+    if (r.includes("Won")) score++;
+    else if (r.includes("Lost")) score--;
+  }
+  return score;
+}
+
+console.log(game(playRound,3));
